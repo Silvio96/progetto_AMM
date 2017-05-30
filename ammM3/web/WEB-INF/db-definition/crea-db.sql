a10d8dda04664fb7e69bfd06a6abf13a346e5c22
Silvio Casula <Silvio Casula@DESKTOP-NFFE9NF.station>
2017-05-30 10:28:59 +0200

    M4

---
+++ ammM3/web/WEB-INF/db-definition/crea-db.sql
@@ -0,0 +1,13 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  Silvio Casula
+ * Created: 19-mag-2017
+ */
+
+/* username : silvio    password: password */
+
+CREATE DATABASE db;
